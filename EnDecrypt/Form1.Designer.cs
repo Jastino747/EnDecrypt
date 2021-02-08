5645c3d2910ddada59cf614321b87f6ec216a6db
Jason A. J <78688089+Jastino747@users.noreply.github.com>
2021-02-08 14:04:11 +0700

    Add files via upload

---
+++ EnDecrypt/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace EnDecrypt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.buttonDecrypt = new System.Windows.Forms.Button();
+            this.buttonEncrypt = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.richTextBoxOutput = new System.Windows.Forms.RichTextBox();
+            this.richTextBoxInput = new System.Windows.Forms.RichTextBox();
+            this.buttonClose2 = new System.Windows.Forms.Button();
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.timerEncrypt = new System.Windows.Forms.Timer(this.components);
+            this.timerDecrypt = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonTerminate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonDecrypt
+            // 
+            this.buttonDecrypt.Font = new System.Drawing.Font("Agency FB", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonDecrypt.Location = new System.Drawing.Point(571, 386);
+            this.buttonDecrypt.Name = "buttonDecrypt";
+            this.buttonDecrypt.Size = new System.Drawing.Size(200, 60);
+            this.buttonDecrypt.TabIndex = 22;
+            this.buttonDecrypt.TabStop = false;
+            this.buttonDecrypt.Text = "Decrypt Text";
+            this.buttonDecrypt.UseVisualStyleBackColor = true;
+            this.buttonDecrypt.Click += new System.EventHandler(this.buttonDecrypt_Click);
+            // 
+            // buttonEncrypt
+            // 
+            this.buttonEncrypt.Font = new System.Drawing.Font("Agency FB", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonEncrypt.Location = new System.Drawing.Point(31, 386);
+            this.buttonEncrypt.Name = "buttonEncrypt";
+            this.buttonEncrypt.Size = new System.Drawing.Size(200, 60);
+            this.buttonEncrypt.TabIndex = 21;
+            this.buttonEncrypt.TabStop = false;
+            this.buttonEncrypt.Text = "Encrypt Text";
+            this.buttonEncrypt.UseVisualStyleBackColor = true;
+            this.buttonEncrypt.Click += new System.EventHandler(this.buttonEncrypt_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(725, 477);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(66, 17);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "OUTPUT";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(742, 60);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 17);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "INPUT";
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Agency FB", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(220, 10);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(360, 55);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "EnDecrypt";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // richTextBoxOutput
+            // 
+            this.richTextBoxOutput.Location = new System.Drawing.Point(0, 500);
+            this.richTextBoxOutput.Name = "richTextBoxOutput";
+            this.richTextBoxOutput.Size = new System.Drawing.Size(800, 300);
+            this.richTextBoxOutput.TabIndex = 17;
+            this.richTextBoxOutput.TabStop = false;
+            this.richTextBoxOutput.Text = "";
+            // 
+            // richTextBoxInput
+            // 
+            this.richTextBoxInput.Location = new System.Drawing.Point(0, 80);
+            this.richTextBoxInput.Name = "richTextBoxInput";
+            this.richTextBoxInput.Size = new System.Drawing.Size(800, 300);
+            this.richTextBoxInput.TabIndex = 14;
+            this.richTextBoxInput.Text = "";
+            // 
+            // buttonClose2
+            // 
+            this.buttonClose2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonClose2.Location = new System.Drawing.Point(0, 0);
+            this.buttonClose2.Name = "buttonClose2";
+            this.buttonClose2.Size = new System.Drawing.Size(40, 40);
+            this.buttonClose2.TabIndex = 16;
+            this.buttonClose2.TabStop = false;
+            this.buttonClose2.Text = "X";
+            this.buttonClose2.UseVisualStyleBackColor = true;
+            this.buttonClose2.Click += new System.EventHandler(this.buttonClose2_Click);
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonClose.Location = new System.Drawing.Point(760, 0);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(40, 40);
+            this.buttonClose.TabIndex = 15;
+            this.buttonClose.TabStop = false;
+            this.buttonClose.Text = "X";
+            this.buttonClose.UseVisualStyleBackColor = true;
+            this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
+            // 
+            // timerEncrypt
+            // 
+            this.timerEncrypt.Interval = 10;
+            this.timerEncrypt.Tick += new System.EventHandler(this.timerEncrypt_Tick);
+            // 
+            // timerDecrypt
+            // 
+            this.timerDecrypt.Interval = 10;
+            this.timerDecrypt.Tick += new System.EventHandler(this.timerDecrypt_Tick);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Agency FB", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(140, 460);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(520, 24);
+            this.label1.TabIndex = 23;
+            this.label1.Text = "This program is case sensitive, so pay attention to your text input.";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // buttonTerminate
+            // 
+            this.buttonTerminate.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.buttonTerminate.Font = new System.Drawing.Font("Agency FB", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonTerminate.Location = new System.Drawing.Point(305, 386);
+            this.buttonTerminate.Name = "buttonTerminate";
+            this.buttonTerminate.Size = new System.Drawing.Size(200, 60);
+            this.buttonTerminate.TabIndex = 24;
+            this.buttonTerminate.TabStop = false;
+            this.buttonTerminate.Text = "Terminate";
+            this.buttonTerminate.UseVisualStyleBackColor = false;
+            this.buttonTerminate.Click += new System.EventHandler(this.buttonTerminate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 800);
+            this.Controls.Add(this.buttonTerminate);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonDecrypt);
+            this.Controls.Add(this.buttonEncrypt);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.richTextBoxOutput);
+            this.Controls.Add(this.richTextBoxInput);
+            this.Controls.Add(this.buttonClose2);
+            this.Controls.Add(this.buttonClose);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonDecrypt;
+        private System.Windows.Forms.Button buttonEncrypt;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox richTextBoxOutput;
+        private System.Windows.Forms.RichTextBox richTextBoxInput;
+        private System.Windows.Forms.Button buttonClose2;
+        private System.Windows.Forms.Button buttonClose;
+        private System.Windows.Forms.Timer timerEncrypt;
+        private System.Windows.Forms.Timer timerDecrypt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonTerminate;
+    }
+}
+
---
+++ EnDecrypt/Form1.cs
@@ -0,0 +1,355 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EnDecrypt
+{
+    public partial class Form1 : Form
+    {
+        public List<ClassData> listData = new List<ClassData>();
+        string text;
+        int manipulator;
+        int subManipulator;
+        int index;
+        Random generator = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.DoubleBuffered = true;
+
+            richTextBoxInput.Text = "Input your text here";
+            richTextBoxOutput.Text = "See your output here";
+
+            this.CenterToScreen();
+
+            InputData("\t", 0, "");
+            InputData("\n", 1, "");
+            InputData(" ", 2, "");
+            InputData("!", 3, "");
+            InputData("\"", 4, "");
+            InputData("#", 5, "");
+            InputData("$", 6, "");
+            InputData("%", 7, "");
+            InputData("&", 8, "");
+            InputData("\'", 9, "");
+            InputData("(", 10, "");
+            InputData(")", 11, "");
+            InputData("*", 12, "");
+            InputData("+", 13, "");
+            InputData(",", 14, "");
+            InputData("-", 15, "");
+            InputData(".", 16, "");
+            InputData("/", 17, "");
+            InputData("0", 18, "");
+            InputData("1", 19, "");
+            InputData("2", 20, "");
+            InputData("3", 21, "");
+            InputData("4", 22, "");
+            InputData("5", 23, "");
+            InputData("6", 24, "");
+            InputData("7", 25, "");
+            InputData("8", 26, "");
+            InputData("9", 27, "");
+            InputData(":", 28, "");
+            InputData(";", 29, "");
+            InputData("<", 30, "");
+            InputData("=", 31, "");
+            InputData(">", 32, "");
+            InputData("?", 33, "");
+            InputData("@", 34, "");
+            InputData("A", 35, "");
+            InputData("B", 36, "");
+            InputData("C", 37, "");
+            InputData("D", 38, "");
+            InputData("E", 39, "");
+            InputData("F", 40, "");
+            InputData("G", 41, "");
+            InputData("H", 42, "");
+            InputData("I", 43, "");
+            InputData("J", 44, "");
+            InputData("K", 45, "");
+            InputData("L", 46, "");
+            InputData("M", 47, "");
+            InputData("N", 48, "");
+            InputData("O", 49, "");
+            InputData("P", 50, "");
+            InputData("Q", 51, "");
+            InputData("R", 52, "");
+            InputData("S", 53, "");
+            InputData("T", 54, "");
+            InputData("U", 55, "");
+            InputData("V", 56, "");
+            InputData("W", 57, "");
+            InputData("X", 58, "");
+            InputData("Y", 59, "");
+            InputData("Z", 60, "");
+            InputData("[", 61, "");
+            InputData("\\",62, "");
+            InputData("]", 63, "");
+            InputData("^", 64, "");
+            InputData("_", 65, "");
+            InputData("`", 66, "");
+            InputData("a", 67, "");
+            InputData("b", 68, "");
+            InputData("c", 69, "");
+            InputData("d", 70, "");
+            InputData("e", 71, "");
+            InputData("f", 72, "");
+            InputData("g", 73, "");
+            InputData("h", 74, "");
+            InputData("i", 75, "");
+            InputData("j", 76, "");
+            InputData("k", 77, "");
+            InputData("l", 78, "");
+            InputData("m", 79, "");
+            InputData("n", 80, "");
+            InputData("o", 81, "");
+            InputData("p", 82, "");
+            InputData("q", 83, "");
+            InputData("r", 84, "");
+            InputData("s", 85, "");
+            InputData("t", 86, "");
+            InputData("u", 87, "");
+            InputData("v", 88, "");
+            InputData("w", 89, "");
+            InputData("x", 90, "");
+            InputData("y", 91, "");
+            InputData("z", 92, "");
+            InputData("{", 93, "");
+            InputData("|", 94, "");
+            InputData("}", 95, "");
+            InputData("~", 96, "");
+        }
+
+        private void buttonClose2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            // Application.Restart();
+        }
+
+        private void buttonClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void buttonEncrypt_Click(object sender, EventArgs e)
+        {
+            RefreshData();
+            manipulator = 0;
+            subManipulator = 0;
+            richTextBoxOutput.Clear();
+            text = richTextBoxInput.Text;
+            richTextBoxInput.Enabled = false;
+            richTextBoxOutput.Enabled = false;
+            buttonDecrypt.Enabled = false;
+            buttonEncrypt.Enabled = false;
+            for (int i = 0; i < listData.Count; i++)
+            {
+                GenerateCode(i);
+            }
+            /*for (int i=0; i<listData.Count; i++)
+            {
+                for (int j=0; j<listData.Count; j++)
+                {
+                    if (int.Parse(listData[i].Shuf) == j)
+                    {
+                        richTextBoxOutput.Text += listData[j].Cha;
+                    }
+                }
+            }*/
+            index = 0;
+            timerEncrypt.Enabled = true;
+        }
+
+        private void buttonDecrypt_Click(object sender, EventArgs e)
+        {
+            RefreshData();
+            manipulator = 0;
+            subManipulator = 0;
+            richTextBoxOutput.Clear();
+            text = richTextBoxInput.Text;
+            richTextBoxInput.Enabled = false;
+            richTextBoxOutput.Enabled = false;
+            buttonDecrypt.Enabled = false;
+            buttonEncrypt.Enabled = false;
+            for (int i = 96; i >= 0; i--)
+            {
+                try
+                {
+                    DegenerateCode(i);
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Decryption failed due to incompetence of encrypted text input. \nPay attention to each characters, including upper case, lower case, spaces even tabs and enters.");
+                    break;
+                }
+            }
+            timerDecrypt.Enabled = true;
+        }
+        private void InputData(string ch, int id, string sh)
+        {
+            ClassData data = new ClassData(ch, id, sh);
+            listData.Add(data);
+        }
+
+        private void timerEncrypt_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                if (text.Length > 0)
+                {
+                    subManipulator = subManipulator + subManipulator + 1;
+                    manipulator += subManipulator;
+                    if (manipulator > 96) manipulator = manipulator % 97;
+                    for (int i = 0; i < listData.Count; i++)
+                    {
+                        if (text.Substring(0, 1) == listData[i].Cha)
+                        {
+                            for (int j = 0; j < listData.Count; j++)
+                            {
+                                if (int.Parse(listData[i].Shuf) == j)
+                                {
+                                    richTextBoxOutput.Text += listData[(j + manipulator) % 97].Cha;
+                                    text = text.Remove(0, 1);
+                                    break;
+                                }
+                            }
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i < listData.Count; i++)
+                    {
+                        if (index == i)
+                        {
+                            for (int j = 0; j < listData.Count; j++)
+                            {
+                                if (int.Parse(listData[i].Shuf) == j)
+                                {
+                                    /*richTextBoxOutput.Text += "\n";
+                                    richTextBoxOutput.Text += (listData[i].Cha + " = " + listData[j].Cha);*/
+                                    richTextBoxOutput.Text += listData[j].Cha;
+                                }
+                            }
+                        }
+                    }
+                    index++;
+                    if (index > 96)
+                    {
+                        index = 0;
+                        richTextBoxInput.Enabled = true;
+                        richTextBoxOutput.Enabled = true;
+                        buttonDecrypt.Enabled = true;
+                        buttonEncrypt.Enabled = true;
+                        RefreshData();
+                        timerEncrypt.Enabled = false;
+                        text = "";
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Encryption failed due to incompetence of encrypted text input. \nPay attention to each characters, including upper case, lower case, spaces even tabs and enters.");
+            }
+        }
+
+        private void timerDecrypt_Tick(object sender, EventArgs e)
+        {
+            if (text.Length > 0)
+            {
+                subManipulator = subManipulator + subManipulator + 1;
+                manipulator += subManipulator;
+                if (manipulator > 96) manipulator = manipulator % 97;
+                for (int i = 0; i < listData.Count; i++)
+                {
+                    if (text.Substring(0, 1) == listData[(i + manipulator) % 97].Cha)
+                    {
+                        for (int j = 0; j < listData.Count; j++)
+                        {
+                            if (int.Parse(listData[j].Shuf) == i)
+                            {
+                                richTextBoxOutput.Text += listData[j].Cha;
+                                text = text.Remove(0, 1);
+                                break;
+                            }
+                        }
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                richTextBoxInput.Enabled = true;
+                richTextBoxOutput.Enabled = true;
+                buttonDecrypt.Enabled = true;
+                buttonEncrypt.Enabled = true;
+                RefreshData();
+                timerDecrypt.Enabled = false;
+                text = "";
+            }
+        }
+        private void GenerateCode(int ind)
+        {
+            listData[ind].Shuf = generator.Next(0, 97).ToString();
+            bool valid = true;
+            for (int i = 0; i < listData.Count; i++)
+            {
+                if (listData[i].Shuf == listData[ind].Shuf && i != ind)
+                {
+                    valid = false;
+                    listData[ind].Shuf = "";
+                    break;
+                }
+            }
+            if (valid == false)
+            {
+                GenerateCode(ind);
+            }
+        }
+        private void DegenerateCode(int ind)
+        {
+            for (int i=0; i<listData.Count; i++)
+            {
+                if (text.Substring(text.Length - 1, 1) == listData[i].Cha)
+                {
+                    listData[ind].Shuf = i.ToString();
+                    text = text.Remove(text.Length - 1, 1);
+                    /*richTextBoxOutput.Text += "\n";
+                    richTextBoxOutput.Text += (listData[ind].Cha + " = " + listData[i].Cha);*/
+                    break;
+                }
+            }
+        }
+        private void RefreshData()
+        {
+            for (int i=0; i < listData.Count; i++)
+            {
+                listData[i].Shuf = "";
+            }
+        }
+
+        private void buttonTerminate_Click(object sender, EventArgs e)
+        {
+            richTextBoxInput.Enabled = true;
+            richTextBoxOutput.Enabled = true;
+            buttonDecrypt.Enabled = true;
+            buttonEncrypt.Enabled = true;
+            RefreshData();
+            timerDecrypt.Enabled = false;
+            timerEncrypt.Enabled = false;
+        }
+    }
+}
